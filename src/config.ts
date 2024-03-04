ef9f1a666909bdff0f1f71d396ff5bb38a1e403e
Sasha Zhegulov <zhegulov.sasha@gmail.com>
2024-03-04 16:33:21 +0300

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://api.valantis.store:40000/';
-export const API_PASS = 'Valantis';
+export const API_URL = "https://api.valantis.store:41000/";
+export const API_PASS = "Valantis";
 
 export const SHOW_LIMIT = 50;
